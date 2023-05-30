4de68675aca04c6bccce111d46fb2b135905b8dd
Alfredo Aguiar Arce <alfredoaguiararce@gmail.com>
2023-05-30 00:19:15 -0700

    :construction: Add basic methods to work with the open ai api

---
+++ Partner.Core/Models/PartnerCoreConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI.API.Models;
+
+namespace Partner.Core.Models;
+public class PartnerCoreConfiguration
+{
+    public string? OPENAI_KEY { get; set; }
+    public int? MAX_TOKENS { get; set; }
+    public double? TEMPERATURE { get; set; }
+    public Model? MODEL { get; set; }
+}
