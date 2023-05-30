59b742f7ea05a9f6f580ccb050eaa47dd3403c97
Alfredo Aguiar Arce <alfredoaguiararce@gmail.com>
2023-05-29 23:54:48 -0700

    :construction: Add basic structure

---
+++ Partner.Core/Partner.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Partner.Core
+{
+    public static class Partner
+    {
+        public static void PartnerCore(this IServiceCollection Services)
+        {
+            // Use this method to inject servcices from the library 
+        }
+    }
+}
